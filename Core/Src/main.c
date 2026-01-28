02d66087587bcaf8c28c3bf2591acd4763e6a581
n.eremeev <ron05051980@gmail.com>
2026-01-28 15:50:55 +0200

    fixes pulls

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -767,7 +767,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin : Break_K_p_Pin */
   GPIO_InitStruct.Pin = Break_K_p_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(Break_K_p_GPIO_Port, &GPIO_InitStruct);
 
